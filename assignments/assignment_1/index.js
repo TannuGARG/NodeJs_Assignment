e27798a181837f2f99a703bf880dd1d8a409723a
Tamanna garg <tgarg661@gmail.com>
2021-12-29 13:08:18 +0530

    testcases passed

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,17 +1,23 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
-    process.argv=[...process.argv,"Yash"]
-    return process.argv[2]
+    // process.argv=[...process.argv,"Yash"]
+    // return process.argv[2]
+    return process.argv[2];
     
 }
 
 function getNameFromEnv() {
-    process.env.name= "Yash";
+    // process.env.name= "Yash";
     return process.env.name
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline=require("readline");
+    const element=readline.createInterface({input:process.stdin,output:process.stdout});
+    element.question((userInput)=>{
+        return userInput;
+    })
     
 }
 
